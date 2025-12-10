7a037635db5222b42e38e8cef39f7e90e8530cf3
Chanawin-kmpn <pond_chanawin@hotmail.com>
2025-12-10 21:09:33 +0700

    update: update features illustration

--- constants/constant.ts
+++ constants/constant.ts
@@ -1,1 +1,6 @@
 export const TWENTY_MB_IN_BYTES = 20 * 1024 * 1024;
+
+export const MIN_LURMTAR_TIME = 2;
+export const MAX_LURMTAR_TIME = 6;
+export const MAX_SCALE = 1.8;
+export const MAX_DELAY = 1.5;
